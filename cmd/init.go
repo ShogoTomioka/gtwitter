9583372df63b30dd49ec23c4bed5ee4368ffafa9
tomiokashougo <tomiokasyogo@gmail.com>
2019-02-19 21:23:16 +0900

    タイムラインを表示するところまで

--- cmd/init.go
+++ cmd/init.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/urfave/cli"
 )
 
@@ -25,10 +23,7 @@ func init() {
 					Value: "20",
 					Usage: "set the limit of timelines, default is 20",
 				}},
-			Action: func(c *cli.Context) error {
-				fmt.Println("completed task: ", c.Args().First())
-				return nil
-			},
+			Action: GetTimeline,
 		},
 		{
 			Name:  "trend",
